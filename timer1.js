4af0e500738ef712791ba7e1e271a6a27dc25958
Giordano Temple <giordanotemple@gmail.com>
2023-01-17 00:07:00 +0000

    linted

--- timer1.js
+++ timer1.js
@@ -1,20 +1,20 @@
 const timer = (...args) => {
-  const times = args.sort() //not entirely necessary but this would ensure that the seconds are as exact as possible within the stack
+  const times = args.sort(); //not entirely necessary but this would ensure that the seconds are as exact as possible within the stack
 
   for (let time of times) {
     if (time < 0 || typeof time !== 'number') continue;
     
-    seconds = time * 1000
-    setTimeout((t) => {
+    let seconds = time * 1000;
+    setTimeout(() => {
       console.log(`It has been ${time} seconds`);
       process.stdout.write('\x07');
-    }, seconds)
+    }, seconds);
   }
-}
+};
 
 
-timer(4, 3, 1, 8, 9)
+timer(4, 3, 1, 8, 9);
 
-timer(4, 'apple', 1, false, 9)
+timer(4, 'apple', 1, false, 9);
 
-timer(4, 3, 1, -7, 9)
+timer(4, 3, 1, -7, 9);
