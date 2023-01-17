4dc0d91851e8f060a303e29887a6e52fe1769704
Giordano Temple <giordanotemple@gmail.com>
2023-01-17 00:05:31 +0000

    complete

---
+++ timer1.js
@@ -0,0 +1,20 @@
+const timer = (...args) => {
+  const times = args.sort() //not entirely necessary but this would ensure that the seconds are as exact as possible within the stack
+
+  for (let time of times) {
+    if (time < 0 || typeof time !== 'number') continue;
+    
+    seconds = time * 1000
+    setTimeout((t) => {
+      console.log(`It has been ${time} seconds`);
+      process.stdout.write('\x07');
+    }, seconds)
+  }
+}
+
+
+timer(4, 3, 1, 8, 9)
+
+timer(4, 'apple', 1, false, 9)
+
+timer(4, 3, 1, -7, 9)
